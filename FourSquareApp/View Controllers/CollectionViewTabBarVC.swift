1988caf6d8a0b2bb413d7eda27eb21bbba6e16a5
phoenixmcknight <phoenixmcknight@gmail.com>
2019-11-13 10:00:54 -0500

    moves create collectionCell functionality from VenueCollectionVC to CollectionViewTabBarVC

--- FourSquareApp/View Controllers/CollectionViewTabBarVC.swift
+++ FourSquareApp/View Controllers/CollectionViewTabBarVC.swift
@@ -13,12 +13,19 @@ import UIKit
 class CollectionViewTabBarVC: UIViewController {
         var savedCollection = [CreateVenueCollection]() {
             didSet {
-                print("receivedData")
-                print(self.savedCollection.count)
                 venueCollectionView.reloadData()
             }
         }
         
+    lazy var collectionViewNameTextField:UITextField = {
+          let ttf = UITextField()
+          ttf.placeholder = "Enter a Title For a New Collection Here"
+          ttf.textAlignment = .center
+          ttf.textColor = .black
+          ttf.borderStyle = .roundedRect
+        ttf.backgroundColor = .clear
+          return ttf
+      }()
 
         lazy var venueCollectionView:UICollectionView = {
              let layout = UICollectionViewFlowLayout(placeHolder: "placeholder")
@@ -33,10 +40,12 @@ class CollectionViewTabBarVC: UIViewController {
                let sbo = UISearchBar()
                sbo.tag = 0
               sbo.backgroundColor = .clear
-               sbo.barTintColor = .clear
+        sbo.placeholder = "Search"
               sbo.searchBarStyle = .minimal
                return sbo
            }()
+    
+    lazy var outletArray = [self.collectionViewNameTextField,self.searchBarOne,self.venueCollectionView]
     
     var searchString:String? = nil {
         didSet {
@@ -52,17 +61,18 @@ class CollectionViewTabBarVC: UIViewController {
         return savedCollection.filter({$0.name.lowercased().contains(search.lowercased())})
     }
     
+    var collectionViewImage = UIImage(systemName: "photo")
         
         override func viewDidLoad() {
             super.viewDidLoad()
             view.backgroundColor = .white
-            addToViewSubview()
             configureConstraints()
             venueCollectionView.dataSource = self
             venueCollectionView.delegate = self
             searchBarOne.delegate = self
             gradientColorBackGrounds()
             navigationItem.title = "Venue Collection"
+             navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(create))
         }
         
         
@@ -71,16 +81,66 @@ class CollectionViewTabBarVC: UIViewController {
             loadData()
          
         }
-    private func addToViewSubview() {
-        view.addSubview(searchBarOne)
-        view.addSubview(venueCollectionView)
+    @objc private func create() {
+        guard collectionViewNameTextField.hasText else {
+            alreadySavedOrCreatedAlert(title: "⚠️ Warning ⚠️", message: "Invalid Text Field Entry")
+            return}
+        guard !savedCollection.contains(where: {$0.name == collectionViewNameTextField.text})
+            else {
+            alreadySavedOrCreatedAlert(title:"⚠️ Warning ⚠️" , message: "A Collection With That Name Already Exists")
+                return
+                
+        }
+        
+            presentVenuePhotoVC()
+        
+//            let newCollection = CreateVenueCollection(name: collectionViewNameTextField.text!, image: (collectionViewImage?.pngData())!,savedVenue:[] )
+//
+//        try? VenueCollectionPersistenceManager.manager.save(newCollection: newCollection)
+//        collectionViewImage = UIImage(systemName: "photo")
+//        loadData()
+        }
+    
+    private func presentVenuePhotoVC() {
+        let alert = UIAlertController(title: "Picture Type", message: "", preferredStyle: .actionSheet)
+        let customPicture = UIAlertAction(title: "Use Custom Picture", style: .default) { [weak self] (result) in
+            
+            let pictureVC = VenuePhotoCollectionVC()
+            pictureVC.delegate = self
+            self?.dismiss(animated: true) {
+                self?.present(pictureVC,animated: true)
+            }
+           
+        }
+        let useDefaultImage = UIAlertAction(title: "Use Default Image", style: .cancel) { [weak self] (result) in
+            let newCollection = CreateVenueCollection(name: (self?.collectionViewNameTextField.text!)!, image: (self?.collectionViewImage?.pngData())!,savedVenue:[] )
+              
+              try? VenueCollectionPersistenceManager.manager.save(newCollection: newCollection)
+            self?.collectionViewImage = UIImage(systemName: "photo")
+            self?.loadData()
+        }
+//        let useDefaultImage = UIAlertAction(title: "Use Default Image", style: .cancel, handler: nil)
+        alert.addAction(customPicture)
+        alert.addAction(useDefaultImage)
+        present(alert,animated: true)
+        
     }
+    
         private func configureConstraints() {
-            venueCollectionView.translatesAutoresizingMaskIntoConstraints = false
-            searchBarOne.translatesAutoresizingMaskIntoConstraints = false
+            for outlet in outletArray {
+                outlet.translatesAutoresizingMaskIntoConstraints = false
+                view.addSubview(outlet)
+            }
             
             NSLayoutConstraint.activate([
-                searchBarOne.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 20),
+                
+                collectionViewNameTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 20),
+                collectionViewNameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 10),
+                collectionViewNameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -10),
+                
+                collectionViewNameTextField.heightAnchor.constraint(equalToConstant: view.frame.height * 0.040),
+                
+                searchBarOne.topAnchor.constraint(equalTo: collectionViewNameTextField.bottomAnchor,constant: 20),
                 searchBarOne.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 10),
                 searchBarOne.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -10),
                 
@@ -105,7 +165,39 @@ class CollectionViewTabBarVC: UIViewController {
         
         }
     private func gradientColorBackGrounds() {
-        CustomLayer.shared.setGradientBackground(colorTop: .darkGray, colorBottom: .white, newView: view)
+        CustomLayer.shared.setGradientBackground(colorTop: .white, colorBottom: .darkGray, newView: view)
+    }
+    
+      private func alreadySavedOrCreatedAlert(title:String,message:String) {
+
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "Dismiss", style: .cancel, handler: nil)
+        alert.addAction(cancel)
+        present(alert,animated: true)
+    }
+    private func deleteOrLookAtCollectionAlert(indexPath:Int) {
+        let alert = UIAlertController(title: "Options", message: nil, preferredStyle: .actionSheet)
+        
+        let delete = UIAlertAction(title: "Delete", style: .destructive) { [weak self](delete) in
+            self?.savedCollection.remove(at: indexPath)
+            guard let collection = self?.savedCollection else {return}
+            try? VenueCollectionPersistenceManager.manager.replaceAllFunction(newCollection: collection)
+        }
+        let viewTableview = UIAlertAction(title: "Look Inside \(savedCollection[indexPath].name)", style: .default) { [weak self](result) in
+             let listVC = ListTableViewController()
+                       listVC.precedingVC = .collectionVC
+                       listVC.currentIndex = indexPath
+            listVC.collectionTableViewData = self?.savedCollection[indexPath].savedVenue
+            guard let title = self?.savedCollection[indexPath].name else {return}
+            
+            listVC.navigationItem.title = title
+            self?.navigationController?.pushViewController(listVC, animated: true)
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(delete)
+        alert.addAction(viewTableview)
+        alert.addAction(cancel)
+        present(alert,animated: true)
     }
     }
 
@@ -128,12 +220,13 @@ class CollectionViewTabBarVC: UIViewController {
         }
         func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
            
-            let listVC = ListTableViewController()
-            listVC.precedingVC = .collectionVC
-            listVC.currentIndex = indexPath.item
-            listVC.collectionTableViewData = savedCollection[indexPath.item].savedVenue
-            listVC.navigationItem.title = "\(savedCollection[indexPath.item].name) Collection"
-            navigationController?.pushViewController(listVC, animated: true)
+//            let listVC = ListTableViewController()
+//            listVC.precedingVC = .collectionVC
+//            listVC.currentIndex = indexPath.item
+//            listVC.collectionTableViewData = savedCollection[indexPath.item].savedVenue
+//            listVC.navigationItem.title = "\(savedCollection[indexPath.item].name) Collection"
+//            navigationController?.pushViewController(listVC, animated: true)
+            deleteOrLookAtCollectionAlert(indexPath: indexPath.item)
         }
         
     }
